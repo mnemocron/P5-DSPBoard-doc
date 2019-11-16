30341593fbb48d34f72eeaf74b23e40f5ce90541
Simon Burkhardt <simon.burkhardt.prime@gmail.com>
2019-11-16 14:42:50 +0100

    irgendöppis

---
+++ clean.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+rm *.aux > /dev/null 2>&1
+rm *.log > /dev/null 2>&1
+rm *.bcf > /dev/null 2>&1
+rm *.out > /dev/null 2>&1
+rm *.run.xml > /dev/null 2>&1
+rm *.toc > /dev/null 2>&1
+rm *.synctex.gz > /dev/null 2>&1
+
